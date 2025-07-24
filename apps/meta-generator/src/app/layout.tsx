79a8f6af11c667f88724d79ec0368da573b30f1d
hjcho <pj.hjcho@kalmate.net>
2025-07-24 16:37:24 +0900

    feat: Bing / naver meta tag add

--- apps/meta-generator/src/app/layout.tsx
+++ apps/meta-generator/src/app/layout.tsx
@@ -100,8 +100,8 @@ export const metadata: Metadata = {
   verification: {
     google: 'NtDt2jnqkwLbHi1k1NXyUCVEeIlUXhnwF82bf_3bgbY',
     other: {
-      'naver-site-verification': 'naver-webmaster-verification',
-      'msvalidate.01': 'bing-webmaster-verification',
+      'naver-site-verification': 'f15442a32b31aaee5a69ce6d567c1f0ef7645207',
+      'msvalidate.01': 'B686D7507360E4F71A5FB51889F3D858',
     },
   },
 };
--- apps/regax/src/app/layout.tsx
+++ apps/regax/src/app/layout.tsx
@@ -105,8 +105,8 @@ export const metadata: Metadata = {
   verification: {
     google: 'NtDt2jnqkwLbHi1k1NXyUCVEeIlUXhnwF82bf_3bgbY',
     other: {
-      'naver-site-verification': 'your-naver-verification-code',
-      'msvalidate.01': 'your-bing-verification-code',
+      'naver-site-verification': 'f15442a32b31aaee5a69ce6d567c1f0ef7645207',
+      'msvalidate.01': 'B686D7507360E4F71A5FB51889F3D858',
     },
   },
 };
