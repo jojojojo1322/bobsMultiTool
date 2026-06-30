e7167460227b03b2d51969d6f410c30de9d4abd2
조현재 <hjcho@hist.co.kr>
2026-06-30 16:19:41 +0900

    Add Naver site verification meta

--- apps/main/src/app/layout.tsx
+++ apps/main/src/app/layout.tsx
@@ -56,6 +56,9 @@ export const metadata: Metadata = {
   },
   verification: {
     google: "NtDt2jnqkwLbHi1k1NXyUCVEeIlUXhnwF82bf_3bgbY",
+    other: {
+      "naver-site-verification": "f15442a32b31aaee5a69ce6d567c1f0ef7645207",
+    },
   },
 };
 
